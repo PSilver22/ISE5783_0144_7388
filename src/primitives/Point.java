051e3c39dae08b0d035a881b92741a4de176ce36
PSilver22 <75566318+PSilver22@users.noreply.github.com>
2023-02-25 22:10:30 +0200

    Add Ray, Point and Vector classes.

---
+++ src/primitives/Point.java
@@ -0,0 +1,77 @@
+package primitives;
+
+/** Class representing a point in 3D space
+ * @author Pinny Silver
+ */
+public class Point {
+    /** x, y, z coordinate tuple */
+    Double3 xyz;
+
+    /** Constructor which initializes the point with a 3-tuple
+     * @param xyz 3-tuple with x, y, z coordinates
+     */
+    public Point(Double3 xyz) { this.xyz = xyz; }
+
+    /**
+     * Constructor which initializes the point with 3 doubles for each coordinate
+     * @param x x-axis offset
+     * @param y y-axis offset
+     * @param z z-axis offset
+     */
+    public Point(double x, double y, double z) {
+        this(new Double3(x, y, z));
+    }
+
+    /**
+     * Creates a new Point moved in the direction and magnitude of the operand Vector
+     * @param op The Vector operand
+     * @return New Point which is the result from the addition
+     */
+    public Point add(Vector op) {
+        return new Point(xyz.add(op.xyz));
+    }
+
+    /**
+     * Creates a new Point where each coordinate is the subtraction of the respective coordinates of op from this
+     * @param op Point to subtract from this
+     * @return New Point which is the result from the subtraction
+     */
+    public Point subtract(Point op) { return new Point(this.xyz.subtract(op.xyz)); }
+
+    /**
+     * Finds the distance squared between this and op
+     * @param op Point to find the distance squared from
+     * @return Distance between this and op, squared
+     */
+    public double distanceSquared(Point op) {
+        // (x1 - x0)^2 + (y1 - y0)^2 + (z1 - z0)^2
+        Double3 diff = op.xyz.subtract(xyz);
+        Double3 prod = diff.product(diff);
+
+        return prod.d1 + prod.d2 + prod.d3;
+    }
+
+    /**
+     * Finds the distance between this and op
+     * @param op Point to find the distance from
+     * @return Distance between this and op
+     */
+    public double distance(Point op) {
+        double dSq = distanceSquared(op);
+        return Math.sqrt(dSq);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+
+        if (o instanceof Point p) {
+            return xyz.equals(p.xyz);
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() { return xyz.toString(); }
+}
