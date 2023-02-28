922bbc359dd5a33e795b0eee04b97364cac4b921
Yosef100 <60045365+Yosef100@users.noreply.github.com>
2023-02-28 18:14:30 +0200

    Update Geometry.java

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -5,7 +5,7 @@ import primitives.Vector;
 
 /**Geometry class is used for various geometries in three-dimensional cartesian space
  * @author: Yossi Tyberg
- */
+ */ 
 public interface Geometry {
     /**
      * Finds the normal vector from the geometry at a point
