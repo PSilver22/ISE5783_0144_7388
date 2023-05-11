abddafcf7d89f1cef6c206697c5d911dfb0de8bb
yossi tyberg <tybergyossi@gmail.com>
2023-05-11 17:06:18 +0300

    Lights tests

--- src/primitives/Material.java
+++ src/primitives/Material.java
@@ -1,9 +1,15 @@
 package primitives;
 
 public class Material {
+    //attenuation coefficient of diffused and shiny light
     public Double3 kD = Double3.ZERO,kS = Double3.ZERO;
+    //shininess of the material
     public int nShininess = 0;
 
+
+    //Setter methods use builder pattern - return Material
+
+    //Double3 fields have two setters - one using Double3 and using for double
     public Material setKd(Double3 kD) {
         this.kD = kD;
         return this;
